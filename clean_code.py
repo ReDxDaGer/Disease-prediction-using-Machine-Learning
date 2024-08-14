3a7abc8a8aeeb26fc60cfce1dc4bfc84a13ca905
ReDxDaGer <yashmehrotra45@gmail.com>
2024-08-14 12:56:15 +0530

    Added the Error Handeling

--- clean_code.py
+++ clean_code.py
@@ -1,200 +1,195 @@
+import tkinter as tk
 from tkinter import *
+from tkinter import messagebox
 import numpy as np
 import pandas as pd
-# from gui_stuff import *
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+import logging
+import hashlib
+import os
+import json
+from cryptography.fernet import Fernet
+import re
+import tempfile
 
-l1=['back_pain','constipation','abdominal_pain','diarrhoea','mild_fever','yellow_urine',
-'yellowing_of_eyes','acute_liver_failure','fluid_overload','swelling_of_stomach',
-'swelled_lymph_nodes','malaise','blurred_and_distorted_vision','phlegm','throat_irritation',
-'redness_of_eyes','sinus_pressure','runny_nose','congestion','chest_pain','weakness_in_limbs',
-'fast_heart_rate','pain_during_bowel_movements','pain_in_anal_region','bloody_stool',
-'irritation_in_anus','neck_pain','dizziness','cramps','bruising','obesity','swollen_legs',
-'swollen_blood_vessels','puffy_face_and_eyes','enlarged_thyroid','brittle_nails',
-'swollen_extremeties','excessive_hunger','extra_marital_contacts','drying_and_tingling_lips',
-'slurred_speech','knee_pain','hip_joint_pain','muscle_weakness','stiff_neck','swelling_joints',
-'movement_stiffness','spinning_movements','loss_of_balance','unsteadiness',
-'weakness_of_one_body_side','loss_of_smell','bladder_discomfort','foul_smell_of urine',
-'continuous_feel_of_urine','passage_of_gases','internal_itching','toxic_look_(typhos)',
-'depression','irritability','muscle_pain','altered_sensorium','red_spots_over_body','belly_pain',
-'abnormal_menstruation','dischromic _patches','watering_from_eyes','increased_appetite','polyuria','family_history','mucoid_sputum',
-'rusty_sputum','lack_of_concentration','visual_disturbances','receiving_blood_transfusion',
-'receiving_unsterile_injections','coma','stomach_bleeding','distention_of_abdomen',
-'history_of_alcohol_consumption','fluid_overload','blood_in_sputum','prominent_veins_on_calf',
-'palpitations','painful_walking','pus_filled_pimples','blackheads','scurring','skin_peeling',
-'silver_like_dusting','small_dents_in_nails','inflammatory_nails','blister','red_sore_around_nose',
-'yellow_crust_ooze']
+# Setup logging
+logging.basicConfig(filename='disease_predictor.log', level=logging.ERROR, 
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
-disease=['Fungal infection','Allergy','GERD','Chronic cholestasis','Drug Reaction',
-'Peptic ulcer diseae','AIDS','Diabetes','Gastroenteritis','Bronchial Asthma','Hypertension',
-' Migraine','Cervical spondylosis',
-'Paralysis (brain hemorrhage)','Jaundice','Malaria','Chicken pox','Dengue','Typhoid','hepatitis A',
-'Hepatitis B','Hepatitis C','Hepatitis D','Hepatitis E','Alcoholic hepatitis','Tuberculosis',
-'Common Cold','Pneumonia','Dimorphic hemmorhoids(piles)',
-'Heartattack','Varicoseveins','Hypothyroidism','Hyperthyroidism','Hypoglycemia','Osteoarthristis',
-'Arthritis','(vertigo) Paroymsal  Positional Vertigo','Acne','Urinary tract infection','Psoriasis',
-'Impetigo']
+# List of symptoms and diseases
+l1 = ['back_pain', 'constipation', 'abdominal_pain', 'diarrhoea', 'mild_fever', 'yellow_urine',
+      'yellowing_of_eyes', 'acute_liver_failure', 'fluid_overload', 'swelling_of_stomach',
+      'swelled_lymph_nodes', 'malaise', 'blurred_and_distorted_vision', 'phlegm', 'throat_irritation',
+      'redness_of_eyes', 'sinus_pressure', 'runny_nose', 'congestion', 'chest_pain', 'weakness_in_limbs',
+      'fast_heart_rate', 'pain_during_bowel_movements', 'pain_in_anal_region', 'bloody_stool',
+      'irritation_in_anus', 'neck_pain', 'dizziness', 'cramps', 'bruising', 'obesity', 'swollen_legs',
+      'swollen_blood_vessels', 'puffy_face_and_eyes', 'enlarged_thyroid', 'brittle_nails',
+      'swollen_extremeties', 'excessive_hunger', 'extra_marital_contacts', 'drying_and_tingling_lips',
+      'slurred_speech', 'knee_pain', 'hip_joint_pain', 'muscle_weakness', 'stiff_neck', 'swelling_joints',
+      'movement_stiffness', 'spinning_movements', 'loss_of_balance', 'unsteadiness',
+      'weakness_of_one_body_side', 'loss_of_smell', 'bladder_discomfort', 'foul_smell_of urine',
+      'continuous_feel_of_urine', 'passage_of_gases', 'internal_itching', 'toxic_look_(typhos)',
+      'depression', 'irritability', 'muscle_pain', 'altered_sensorium', 'red_spots_over_body', 'belly_pain',
+      'abnormal_menstruation', 'dischromic _patches', 'watering_from_eyes', 'increased_appetite', 'polyuria', 'family_history', 'mucoid_sputum',
+      'rusty_sputum', 'lack_of_concentration', 'visual_disturbances', 'receiving_blood_transfusion',
+      'receiving_unsterile_injections', 'coma', 'stomach_bleeding', 'distention_of_abdomen',
+      'history_of_alcohol_consumption', 'fluid_overload', 'blood_in_sputum', 'prominent_veins_on_calf',
+      'palpitations', 'painful_walking', 'pus_filled_pimples', 'blackheads', 'scurring', 'skin_peeling',
+      'silver_like_dusting', 'small_dents_in_nails', 'inflammatory_nails', 'blister', 'red_sore_around_nose',
+      'yellow_crust_ooze']
 
-l2=[]
-for x in range(0,len(l1)):
-    l2.append(0)
+disease = ['Fungal infection', 'Allergy', 'GERD', 'Chronic cholestasis', 'Drug Reaction',
+           'Peptic ulcer diseae', 'AIDS', 'Diabetes', 'Gastroenteritis', 'Bronchial Asthma', 'Hypertension',
+           'Migraine', 'Cervical spondylosis', 'Paralysis (brain hemorrhage)', 'Jaundice', 'Malaria', 'Chicken pox',
+           'Dengue', 'Typhoid', 'hepatitis A', 'Hepatitis B', 'Hepatitis C', 'Hepatitis D', 'Hepatitis E',
+           'Alcoholic hepatitis', 'Tuberculosis', 'Common Cold', 'Pneumonia', 'Dimorphic hemmorhoids(piles)',
+           'Heart attack', 'Varicose veins', 'Hypothyroidism', 'Hyperthyroidism', 'Hypoglycemia', 'Osteoarthristis',
+           'Arthritis', '(vertigo) Paroymsal  Positional Vertigo', 'Acne', 'Urinary tract infection', 'Psoriasis',
+           'Impetigo']
 
-# TESTING DATA df -------------------------------------------------------------------------------------
-df=pd.read_csv("Training.csv")
+# Security functions
+def hash_patient_name(name):
+    salt = os.urandom(32)
+    hashed = hashlib.pbkdf2_hmac('sha256', name.encode('utf-8'), salt, 100000)
+    return salt + hashed
 
-df.replace({'prognosis':{'Fungal infection':0,'Allergy':1,'GERD':2,'Chronic cholestasis':3,'Drug Reaction':4,
-'Peptic ulcer diseae':5,'AIDS':6,'Diabetes ':7,'Gastroenteritis':8,'Bronchial Asthma':9,'Hypertension ':10,
-'Migraine':11,'Cervical spondylosis':12,
-'Paralysis (brain hemorrhage)':13,'Jaundice':14,'Malaria':15,'Chicken pox':16,'Dengue':17,'Typhoid':18,'hepatitis A':19,
-'Hepatitis B':20,'Hepatitis C':21,'Hepatitis D':22,'Hepatitis E':23,'Alcoholic hepatitis':24,'Tuberculosis':25,
-'Common Cold':26,'Pneumonia':27,'Dimorphic hemmorhoids(piles)':28,'Heart attack':29,'Varicose veins':30,'Hypothyroidism':31,
-'Hyperthyroidism':32,'Hypoglycemia':33,'Osteoarthristis':34,'Arthritis':35,
-'(vertigo) Paroymsal  Positional Vertigo':36,'Acne':37,'Urinary tract infection':38,'Psoriasis':39,
-'Impetigo':40}},inplace=True)
+def generate_key():
+    return Fernet.generate_key()
 
-# print(df.head())
+def encrypt_data(data, key):
+    f = Fernet(key)
+    return f.encrypt(json.dumps(data).encode())
 
-X= df[l1]
+def decrypt_data(encrypted_data, key):
+    f = Fernet(key)
+    return json.loads(f.decrypt(encrypted_data))
 
-y = df[["prognosis"]]
-np.ravel(y)
-# print(y)
+def sanitize_input(input_string):
+    return re.sub(r'[^a-zA-Z0-9\s]', '', input_string)
 
-# TRAINING DATA tr --------------------------------------------------------------------------------
-tr=pd.read_csv("Testing.csv")
-tr.replace({'prognosis':{'Fungal infection':0,'Allergy':1,'GERD':2,'Chronic cholestasis':3,'Drug Reaction':4,
-'Peptic ulcer diseae':5,'AIDS':6,'Diabetes ':7,'Gastroenteritis':8,'Bronchial Asthma':9,'Hypertension ':10,
-'Migraine':11,'Cervical spondylosis':12,
-'Paralysis (brain hemorrhage)':13,'Jaundice':14,'Malaria':15,'Chicken pox':16,'Dengue':17,'Typhoid':18,'hepatitis A':19,
-'Hepatitis B':20,'Hepatitis C':21,'Hepatitis D':22,'Hepatitis E':23,'Alcoholic hepatitis':24,'Tuberculosis':25,
-'Common Cold':26,'Pneumonia':27,'Dimorphic hemmorhoids(piles)':28,'Heart attack':29,'Varicose veins':30,'Hypothyroidism':31,
-'Hyperthyroidism':32,'Hypoglycemia':33,'Osteoarthristis':34,'Arthritis':35,
-'(vertigo) Paroymsal  Positional Vertigo':36,'Acne':37,'Urinary tract infection':38,'Psoriasis':39,
-'Impetigo':40}},inplace=True)
+def secure_write(data, filename):
+    fd, temp_path = tempfile.mkstemp()
+    try:
+        with os.fdopen(fd, 'w') as temp_file:
+            temp_file.write(data)
+        os.replace(temp_path, filename)
+    except Exception as e:
+        os.remove(temp_path)
+        raise e
 
-X_test= tr[l1]
-y_test = tr[["prognosis"]]
-np.ravel(y_test)
-# ------------------------------------------------------------------------------------------------------
+# Data loading function
+def load_data():
+    try:
+        df = pd.read_csv("Training.csv")
+        tr = pd.read_csv("Testing.csv")
+        
+        # Data preprocessing
+        df.replace({'prognosis': {disease[i]: i for i in range(len(disease))}}, inplace=True)
+        tr.replace({'prognosis': {disease[i]: i for i in range(len(disease))}}, inplace=True)
+        
+        return df, tr
+    except FileNotFoundError:
+        messagebox.showerror("Error", "CSV files not found. Please ensure 'Training.csv' and 'Testing.csv' are in the correct directory.")
+        return None, None
+    except pd.errors.EmptyDataError:
+        messagebox.showerror("Error", "One or both CSV files are empty.")
+        return None, None
+    except Exception as e:
+        logging.error(f"An error occurred while loading data: {str(e)}")
+        messagebox.showerror("Error", "An unexpected error occurred while loading data. Please check the log file.")
+        return None, None
 
+# Load data
+df, tr = load_data()
+if df is None or tr is None:
+    # Handle the error 
+    pass
+else:
+    X = df[l1]
+    y = df[["prognosis"]]
+    X_test = tr[l1]
+    y_test = tr[["prognosis"]]
+
+# Prediction functions
 def DecisionTree():
+    if not validate_inputs():
+        return
+    
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(X, y)
+    
+    y_pred = clf.predict(X_test)
+    print(f"Decision Tree Accuracy: {accuracy_score(y_test, y_pred)}")
+    
+    inputtest = [convert_symptoms_to_binary(get_selected_symptoms())]
+    predicted = clf.predict(inputtest)[0]
+    
+    display_prediction(predicted, t1)
 
-    from sklearn import tree
-
-    clf3 = tree.DecisionTreeClassifier()   # empty model of the decision tree
-    clf3 = clf3.fit(X,y)
-
-    # calculating accuracy-------------------------------------------------------------------
-    from sklearn.metrics import accuracy_score
-    y_pred=clf3.predict(X_test)
-    print(accuracy_score(y_test, y_pred))
-    print(accuracy_score(y_test, y_pred,normalize=False))
-    # -----------------------------------------------------
-
-    psymptoms = [Symptom1.get(),Symptom2.get(),Symptom3.get(),Symptom4.get(),Symptom5.get()]
-
-    for k in range(0,len(l1)):
-        # print (k,)
-        for z in psymptoms:
-            if(z==l1[k]):
-                l2[k]=1
-
-    inputtest = [l2]
-    predict = clf3.predict(inputtest)
-    predicted=predict[0]
-
-    h='no'
-    for a in range(0,len(disease)):
-        if(predicted == a):
-            h='yes'
-            break
-
-
-    if (h=='yes'):
-        t1.delete("1.0", END)
-        t1.insert(END, disease[a])
-    else:
-        t1.delete("1.0", END)
-        t1.insert(END, "Not Found")
-
-
-def randomforest():
-    from sklearn.ensemble import RandomForestClassifier
-    clf4 = RandomForestClassifier()
-    clf4 = clf4.fit(X,np.ravel(y))
-
-    # calculating accuracy-------------------------------------------------------------------
-    from sklearn.metrics import accuracy_score
-    y_pred=clf4.predict(X_test)
-    print(accuracy_score(y_test, y_pred))
-    print(accuracy_score(y_test, y_pred,normalize=False))
-    # -----------------------------------------------------
-
-    psymptoms = [Symptom1.get(),Symptom2.get(),Symptom3.get(),Symptom4.get(),Symptom5.get()]
-
-    for k in range(0,len(l1)):
-        for z in psymptoms:
-            if(z==l1[k]):
-                l2[k]=1
-
-    inputtest = [l2]
-    predict = clf4.predict(inputtest)
-    predicted=predict[0]
-
-    h='no'
-    for a in range(0,len(disease)):
-        if(predicted == a):
-            h='yes'
-            break
-
-    if (h=='yes'):
-        t2.delete("1.0", END)
-        t2.insert(END, disease[a])
-    else:
-        t2.delete("1.0", END)
-        t2.insert(END, "Not Found")
-
+def RandomForest():
+    if not validate_inputs():
+        return
+    
+    clf = RandomForestClassifier()
+    clf = clf.fit(X, np.ravel(y))
+    
+    y_pred = clf.predict(X_test)
+    print(f"Random Forest Accuracy: {accuracy_score(y_test, y_pred)}")
+    
+    inputtest = [convert_symptoms_to_binary(get_selected_symptoms())]
+    predicted = clf.predict(inputtest)[0]
+    
+    display_prediction(predicted, t2)
 
 def NaiveBayes():
-    from sklearn.naive_bayes import GaussianNB
+    if not validate_inputs():
+        return
+    
     gnb = GaussianNB()
-    gnb=gnb.fit(X,np.ravel(y))
+    gnb = gnb.fit(X, np.ravel(y))
+    
+    y_pred = gnb.predict(X_test)
+    print(f"Naive Bayes Accuracy: {accuracy_score(y_test, y_pred)}")
+    
+    inputtest = [convert_symptoms_to_binary(get_selected_symptoms())]
+    predicted = gnb.predict(inputtest)[0]
+    
+    display_prediction(predicted, t3)
 
-    # calculating accuracy-------------------------------------------------------------------
-    from sklearn.metrics import accuracy_score
-    y_pred=gnb.predict(X_test)
-    print(accuracy_score(y_test, y_pred))
-    print(accuracy_score(y_test, y_pred,normalize=False))
-    # -----------------------------------------------------
+# Helper functions
+def validate_inputs():
+    symptoms = get_selected_symptoms()
+    if any(symptom == 'None' for symptom in symptoms):
+        messagebox.showwarning("Incomplete Input", "Please select all 5 symptoms before predicting.")
+        return False
+    return True
 
-    psymptoms = [Symptom1.get(),Symptom2.get(),Symptom3.get(),Symptom4.get(),Symptom5.get()]
-    for k in range(0,len(l1)):
-        for z in psymptoms:
-            if(z==l1[k]):
-                l2[k]=1
+def get_selected_symptoms():
+    return [Symptom1.get(), Symptom2.get(), Symptom3.get(), Symptom4.get(), Symptom5.get()]
 
-    inputtest = [l2]
-    predict = gnb.predict(inputtest)
-    predicted=predict[0]
+def convert_symptoms_to_binary(symptoms):
+    binary_symptoms = [0] * len(l1)
+    for symptom in symptoms:
+        if symptom in l1:
+            binary_symptoms[l1.index(symptom)] = 1
+    return binary_symptoms
 
-    h='no'
-    for a in range(0,len(disease)):
-        if(predicted == a):
-            h='yes'
-            break
-
-    if (h=='yes'):
-        t3.delete("1.0", END)
-        t3.insert(END, disease[a])
+def display_prediction(predicted, text_widget):
+    if 0 <= predicted < len(disease):
+        text_widget.delete("1.0", END)
+        text_widget.insert(END, disease[predicted])
     else:
-        t3.delete("1.0", END)
-        t3.insert(END, "Not Found")
+        text_widget.delete("1.0", END)
+        text_widget.insert(END, "Not Found")
 
-# gui_stuff------------------------------------------------------------------------------------
-
-root = Tk()
+# GUI setup
+root = tk.Tk()
 root.configure(background='blue')
 
-# entry variables
+# Entry variables
 Symptom1 = StringVar()
 Symptom1.set(None)
 Symptom2 = StringVar()
